9e75501c785160a95e40bb2b71f043ffd6ad3fc2
IvanMaras <marasivan2001@gmail.com>
2024-05-22 19:22:32 +0200

    finish

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function calculate() {
-  const num1 = document.getElementById("number1").value;
-  const num2 = document.getElementById("number2").value;
-  const resultBox = document.getElementById("result");
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+  let resultBox = document.getElementById("result");
 
   if (num1 === "" || num2 === "") {
     resultBox.value = "Molim unesite oba broja!";
     resultBox.style.backgroundColor = "red";
   } else {
-    const number1 = parseFloat(num1);
-    const number2 = parseFloat(num2);
+    let number1 = parseFloat(num1);
+    let number2 = parseFloat(num2);
 
-    const result = number1 >= number2 ? number1 : number2;
+    let result = number1 >= number2 ? number1 : number2;
     resultBox.value = result;
     resultBox.style.backgroundColor = "green";
   }
 }
 function randomNumbers() {
-  const randomNum1 = Math.floor(Math.random() * 100);
-  const randomNum2 = Math.floor(Math.random() * 100);
+  let randomNum1 = Math.floor(Math.random() * 100);
+  let randomNum2 = Math.floor(Math.random() * 100);
 
   document.getElementById("number1").value = randomNum1;
   document.getElementById("number2").value = randomNum2;
